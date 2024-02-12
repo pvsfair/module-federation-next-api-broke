a0dcbb54585cffa1d838bf1a11b5d252fdf8dbbd
Paulo Alvares <paulo.alvares@bolttech.io>
2024-02-12 16:25:06 +0000

    feat: added the files
    before they were submodules by mistake 😅

---
+++ module-federation-next-mf-8-1-3/next.config.js
@@ -0,0 +1,25 @@
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "mf-8-1-3",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {},
+        remotes: {},
+        shared: {},
+        extraOptions: {},
+      })
+    );
+    return config;
+  },
+  compiler: {
+    styledComponents: {
+      minify: true,
+      ssr: true,
+    },
+  },
+  // swcMinify: false,
+};
